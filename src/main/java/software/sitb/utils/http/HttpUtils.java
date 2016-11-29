f50986e2699aef95925a3e5455c4cd679e27ca3b
Sean <sean.snow@live.com>
2016-11-29 09:53:54 +0800

    toUrlArgs

---
+++ src/main/java/software/sitb/utils/http/HttpUtils.java
@@ -0,0 +1,39 @@
+package software.sitb.utils.http;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import software.sitb.utils.json.Json;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 田尘殇Sean(sean.snow@live.com) createAt 2016/11/29
+ */
+public class HttpUtils {
+
+
+    /**
+     * 对象转化为url形式的字符串
+     * a=1&b=2
+     *
+     * @param params key，value形式的对象
+     * @return url 字符串
+     */
+    public static String toUrlArgs(Object params) {
+        if (null == params) {
+            return "";
+        }
+
+        JsonNode json = Json.toJson(params);
+        Map<String, String> map = Json.fromJson(json.toString(), new TypeReference<HashMap<String, String>>() {
+        });
+
+        StringBuilder args = new StringBuilder();
+        map.forEach((key, value) -> args.append(key).append("=").append(value).append("&"));
+        args.deleteCharAt(args.length() - 1);
+
+        return args.toString();
+    }
+
+}
